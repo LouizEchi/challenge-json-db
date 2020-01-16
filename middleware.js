baec4b62558d090a735980248ad64d884c70de2a
LouizEchi <louizechiverri@gmail.com>
2020-01-17 01:55:26 +0800

    feat: middleware validations and setters

--- middleware.js
+++ middleware.js
@@ -1,8 +1,46 @@
+const fs = require('fs')
 const { STATUS_CODES } = require('http')
 
 module.exports = {
+  checkIfFileHasData,
+  handleError,
   notFound,
-  handleError
+  validateParameters
+}
+
+function checkIfFileHasData (req, res, next) {
+  try {
+    const { student_id: studentId, parameters } = req.params
+
+    const path = `./data/${studentId}.json`
+
+    if (!fs.existsSync(path)) {
+      return notFound(req, res)
+    }
+
+    const data = JSON.parse(fs.readFileSync(path))
+
+    let hasError
+    parameters.reduce((previous, value) => {
+      if (!previous[value]) {
+        hasError = true
+      }
+
+      return previous[value]
+    }, data)
+
+    if (hasError) {
+      return notFound(req, res)
+    }
+
+    return next()
+  } catch (err) {
+    console.log(err)
+    if (err.statusCode) {
+      return notFound(req, res)
+    }
+    return handleError(err, req, res, next)
+  }
 }
 
 function handleError (err, req, res, next) {
@@ -14,6 +52,20 @@ function handleError (err, req, res, next) {
   res.status(statusCode).json({ error: errorMessage })
 }
 
-function notFound (req, res) {
+function notFound (_, res) {
   res.status(404).json({ error: 'Not Found' })
+}
+
+async function validateParameters (req, res, next) {
+  const params = req.params[0]
+
+  if (!/^((?!\s).)*$/.test(params)) {
+    return res.json({
+      error: 'Invalid params'
+    })
+  }
+
+  req.params.parameters = params.split('/').filter(x => x)
+
+  return next()
 }
