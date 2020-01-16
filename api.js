77b7a6afdb22290a7926d8db735866623ced04cb
LouizEchi <louizechiverri@gmail.com>
2020-01-17 01:55:07 +0800

    feat: main API middleware controllers

--- api.js
+++ api.js
@@ -1,7 +1,51 @@
+const {
+  assignData,
+  retrieveData,
+  removeData,
+  fileRetrieval,
+  fileSave
+} = require('./model')
+
 module.exports = {
-  getHealth
+  getStudent,
+  removeStudent,
+  updateStudent
 }
 
-async function getHealth (req, res, next) {
-  res.json({ success: true })
+async function getStudent (req, res) {
+  const { student_id: studentId, parameters } = req.params
+
+  const path = `./data/${studentId}.json`
+
+  const file = await fileRetrieval(path)
+  const data = await retrieveData(file, parameters)
+
+  res.json({ success: true, data })
+}
+
+async function removeStudent (req, res) {
+  const { student_id: studentId, parameters } = req.params
+
+  const path = `./data/${studentId}.json`
+
+  const data = await fileRetrieval(path)
+  await removeData(data, parameters)
+
+  await fileSave(path, data)
+
+  res.json({ success: true, data })
+}
+
+async function updateStudent (req, res) {
+  const { student_id: studentId, parameters } = req.params
+  const body = req.body
+
+  const path = `./data/${studentId}.json`
+  const data = await fileRetrieval(path)
+
+  await assignData(data, parameters, body)
+
+  await fileSave(path, data)
+
+  res.json({ success: true, data })
 }
